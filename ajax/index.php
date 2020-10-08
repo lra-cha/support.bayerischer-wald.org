cd5dd69468468006cc3c76ae32aeb549dfc29fc9
14wieland <14wieland@PC012-16>
2020-10-08 11:25:37 +0200

    Formular

--- ajax/index.php
+++ ajax/index.php
@@ -1,3 +1,28 @@
 <?php
 
-echo "test";
+if (!(isset($_POST['txt_element'])&&isset($_POST['email'])))
+{
+    echo 'Bitte alle Pflichtfelder ausfüllen';
+    die(0);
+}
+
+// Datenbank verbinden
+$mysqli = mysqli_connect("localhost", "di_check", "di_check", "di_check");
+if (mysqli_connect_errno()) {
+    echo "Fehler beim Speichern";
+}
+else {
+    // Settings schreiben
+    $stmt = $mysqli->prepare("INSERT INTO `settings` (`txt_element`, `firstName`, `lastName`,`email`, `person`, `copyright`,`other`,`ip`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssiiis", $_POST['txt_element'], $_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['person'], $_POST['copyright'], $_POST['other'], $_SERVER['REMOTE_ADDR']);
+    $stmt->execute();
+    if($stmt->affected_rows < 1) {
+        echo "Fehler beim Speichern";
+        die(0);
+    }
+    $stmt->close();
+    $id = $mysqli->insert_id;
+    echo "Ihre Anfrage wurde unter der ID: $id gespeichert!";
+
+    $mysqli->close();
+}
